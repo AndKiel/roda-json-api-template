a3dbb2219a27cd723b7f152542079e552f6f8de6
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-06-13 15:00:56 +0200

    fix: Require JSON

--- config/initializers/blueprinter.rb
+++ config/initializers/blueprinter.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'blueprinter'
+require 'json'
 require 'oj'
 
 Blueprinter.configure do |config|
--- spec/support/rack_test.rb
+++ spec/support/rack_test.rb
@@ -4,4 +4,8 @@ require 'rack/test'
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods, type: :request
+
+  def app
+    RodaJsonApi.app
+  end
 end
