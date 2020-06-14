7355e463efa0b34200dddde36a27be27772c07c3
Andrzej Kiełtyka <a.kieltyka@gmail.com>
2020-06-14 13:47:51 +0200

    chore: Set Content-Type header before each test

--- spec/support/rack_test.rb
+++ spec/support/rack_test.rb
@@ -5,6 +5,10 @@ require 'rack/test'
 RSpec.configure do |config|
   config.include Rack::Test::Methods, type: :request
 
+  config.before do
+    header 'Content-Type', 'application/json'
+  end
+
   def app
     App.app
   end
